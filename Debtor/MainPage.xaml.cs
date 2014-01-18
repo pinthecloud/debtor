a10c7bf811ecd8107ea63c83ed2029e007ea8e62
Seungmin <rfrost77@gmail.com>
2014-01-18 11:08:21 +0900

    m first commit

---
+++ Debtor/MainPage.xaml.cs
@@ -0,0 +1,204 @@
+﻿using Debtor.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Popups;
+
+// 기본 페이지 항목 템플릿에 대한 설명은 http://go.microsoft.com/fwlink/?LinkId=234237에 나와 있습니다.
+
+namespace Debtor
+{
+    /// <summary>
+    /// 대부분의 응용 프로그램에 공통되는 특성을 제공하는 기본 페이지입니다.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        private bool isEventRegistered;
+
+        // Used to determine the correct height to ensure our custom UI fills the screen.
+        private Rect windowBounds;
+
+        // Desired width for the settings UI. UI guidelines specify this should be 346 or 646 depending on your needs.
+        private double settingsWidth = 646;
+
+        // This is the container that will hold our custom content.
+        private Popup settingsPopup;
+
+
+        /// <summary>
+        /// 이는 강력한 형식의 뷰 모델로 변경될 수 있습니다.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper는 각 페이지에서 탐색 및 프로세스 수명 관리를 
+        /// 지원하는 데 사용됩니다.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+
+            // Added to listen for events when the window size is updated.
+            windowBounds = Window.Current.Bounds;
+            Window.Current.SizeChanged += OnWindowSizeChanged;
+
+            // Add Setting Panel
+            SettingsPane.GetForCurrentView().CommandsRequested += App_CommandsRequested;
+            this.isEventRegistered = true;
+        }
+
+        /// <summary>
+        /// Invoked when the window size is updated.
+        /// </summary>
+        /// <param name="sender">Instance that triggered the event.</param>
+        /// <param name="e">Event data describing the conditions that led to the event.</param>
+        void OnWindowSizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            windowBounds = Window.Current.Bounds;
+        }
+
+        void App_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            UICommandInvokedHandler handler = new UICommandInvokedHandler(onSettingsCommand);
+            SettingsCommand privatePolicyCommand = new SettingsCommand("privatePolicy", "개인 정보 취급 방침", handler);
+            args.Request.ApplicationCommands.Add(privatePolicyCommand);
+        }
+
+        void onSettingsCommand(IUICommand command)
+        {
+            // Create a Popup window which will contain our flyout.
+            settingsPopup = new Popup();
+            settingsPopup.Closed += OnPopupClosed;
+            Window.Current.Activated += OnWindowActivated;
+            settingsPopup.IsLightDismissEnabled = true;
+            settingsPopup.Width = settingsWidth;
+            settingsPopup.Height = windowBounds.Height;
+
+            // Add the proper animation for the panel.
+            settingsPopup.ChildTransitions = new TransitionCollection();
+            settingsPopup.ChildTransitions.Add(new PaneThemeTransition()
+            {
+                Edge = (SettingsPane.Edge == SettingsEdgeLocation.Right) ?
+                       EdgeTransitionLocation.Right :
+                       EdgeTransitionLocation.Left
+            });
+
+            // Create a SettingsFlyout the same dimenssions as the Popup.
+            PrivatePolicyFlyout mypane = new PrivatePolicyFlyout();
+            mypane.Width = settingsWidth;
+            mypane.Height = windowBounds.Height;
+
+            // Place the SettingsFlyout inside our Popup window.
+            settingsPopup.Child = mypane;
+
+            // Let's define the location of our Popup.
+            settingsPopup.SetValue(Canvas.LeftProperty, SettingsPane.Edge == SettingsEdgeLocation.Right ? (windowBounds.Width - settingsWidth) : 0);
+            settingsPopup.SetValue(Canvas.TopProperty, 0);
+            settingsPopup.IsOpen = true;
+        }
+
+        /// <summary>
+        /// We use the window's activated event to force closing the Popup since a user maybe interacted with
+        /// something that didn't normally trigger an obvious dismiss.
+        /// </summary>
+        /// <param name="sender">Instance that triggered the event.</param>
+        /// <param name="e">Event data describing the conditions that led to the event.</param>
+        private void OnWindowActivated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
+        {
+            if (e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.Deactivated)
+            {
+                settingsPopup.IsOpen = false;
+            }
+        }
+
+        /// <summary>
+        /// When the Popup closes we no longer need to monitor activation changes.
+        /// </summary>
+        /// <param name="sender">Instance that triggered the event.</param>
+        /// <param name="e">Event data describing the conditions that led to the event.</param>
+        void OnPopupClosed(object sender, object e)
+        {
+            Window.Current.Activated -= OnWindowActivated;
+        }
+
+        /// <summary>
+        /// 탐색 중 전달된 콘텐츠로 페이지를 채웁니다. 이전 세션의 페이지를
+        /// 다시 만들 때 저장된 상태도 제공됩니다.
+        /// </summary>
+        /// <param name="sender">
+        /// 대개 <see cref="NavigationHelper"/>인 이벤트 소스
+        /// </param>
+        /// <param name="e">다음에 전달된 탐색 매개 변수를 제공하는 이벤트 데이터입니다.
+        /// <see cref="Frame.Navigate(Type, Object)"/>에 전달된 매개 변수와
+        /// 이전 세션 동안 이 페이지에 유지된
+        /// 유지됩니다. 페이지를 처음 방문할 때는 이 상태가 null입니다.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// 응용 프로그램이 일시 중지되거나 탐색 캐시에서 페이지가 삭제된 경우
+        /// 이 페이지와 관련된 상태를 유지합니다.  값은
+        /// <see cref="SuspensionManager.SessionState"/>의 serialization 요구 사항을 만족해야 합니다.
+        /// </summary>
+        /// <param name="sender"> 대개 <see cref="NavigationHelper"/>인 이벤트 소스</param>
+        /// <param name="e">serializable 상태로 채워질
+        /// 빈 사전입니다.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper 등록
+
+        /// 이 섹션에서 제공되는 메서드는 NavigationHelper에서
+        /// 페이지의 탐색 메서드에 응답하기 위해 사용됩니다.
+        /// 
+        /// 페이지별 논리는 다음에 대한 이벤트 처리기에 있어야 합니다.  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// 및 <see cref="GridCS.Common.NavigationHelper.SaveState"/>입니다.
+        /// 탐색 매개 변수는 LoadState 메서드 
+        /// 및 이전 세션 동안 유지된 페이지 상태에서 사용할 수 있습니다.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
